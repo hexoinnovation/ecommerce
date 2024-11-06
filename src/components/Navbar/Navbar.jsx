46974aa8bb87fc11861c5de43470d71964f7a911
Nithya1312 <ecommercehexo@gmail.com>
2024-11-06 16:49:37 +0530

    nav changes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -14,41 +14,31 @@ const Menu = [
   },
   {
     id: 2,
-    name: "Top Rated",
-    link: "/#services",
+    name: "Login",
+    link: "/#login",
   },
   {
     id: 3,
-    name: "Kids Wear",
-    link: "/#",
-  },
-  {
-    id: 4, // Changed id to avoid duplication
-    name: "Mens Wear",
-    link: "/#",
-  },
-  {
-    id: 5, // Changed id to avoid duplication
-    name: "Electronics",
-    link: "/#",
+    name: "Contact Us",  // Changed "All Products" to "Contact"
+    link: "/#contact",
   },
 ];
 
 const DropdownLinks = [
   {
     id: 1,
-    name: "Trending Products",
-    link: "/#",
+    name: "Women Wear",
+    link: "/#women-wear",
   },
   {
     id: 2,
-    name: "Best Selling",
-    link: "/#",
+    name: "Men Wear",
+    link: "/#men-wear",
   },
   {
     id: 3,
-    name: "Top Rated",
-    link: "/#",
+    name: "Kids Wear",
+    link: "/#kids-wear",
   },
 ];
 
@@ -65,7 +55,7 @@ const Navbar = ({ handleOrderPopup }) => {
   };
 
   return (
-    <div className="shadow-md bg-red-300 dark:bg-gray-900 dark:text-white duration-200 relative z-40">
+    <div className="shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40">
       {/* upper Navbar */}
       <div className="bg-primary/40 py-2">
         <div className="container flex justify-between items-center">
@@ -131,7 +121,7 @@ const Navbar = ({ handleOrderPopup }) => {
             </li>
           ))}
 
-          {/* Simple Dropdown and Links */}
+          {/* Dropdown for Categories and Contact */}
           <li className="relative">
             <button
               onClick={handleDropdownToggle} // Toggle dropdown on click
