97ca520e4cb2c0435cc150989e75af336903a61e
selva5716 <selvalakshmi5716@gmail.com>
2024-12-09 10:54:02 +0530

    selva

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,31 +1,24 @@
-import React from 'react';
-import { ViewAllProducts } from './ViewAllProducts';
+import React from "react";
 import { Route, Routes, useNavigate } from "react-router-dom"; // Import useNavigate
-import ProductDetail from './ProductDetail';
-import { ProductsProvider } from '../../context/ProductsContext';
+import { ProductsProvider } from "../../context/ProductsContext";
+import ProductDetail from "./ProductDetail";
+import { ViewAllProducts } from "./ViewAllProducts";
 
 export const Products = () => {
-    
-            const navigate = useNavigate();
+  const navigate = useNavigate();
 
-      const handleProductClick = (id) => {
-        navigate(`/product/${id}`);
-      };
-    
-      return (
-        <ProductsProvider>
-        <div className="sm:mt-20 mt-10">
-          <Routes>
-            <Route
-              path="/"
-              element={<ViewAllProducts />}
-            />
-            <Route
-              path="/product/:id"
-              element={<ProductDetail />}
-            />
-          </Routes>
-        </div>
-        </ProductsProvider>
-      );
-    };
+  const handleProductClick = (id) => {
+    navigate(`/product/${id}`);
+  };
+
+  return (
+    <ProductsProvider>
+      <div className="sm:mt-20 mt-10">
+        <Routes>
+          <Route path="/" element={<ViewAllProducts />} />
+          <Route path="/product/:id" element={<ProductDetail />} />
+        </Routes>
+      </div>
+    </ProductsProvider>
+  );
+};
